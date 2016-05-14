0bef074b5f4dae4c1c69cbf4fafe9708554cfdaa
mac50 <mfuller@ieee.org>
2016-05-14 12:06:08 -0400

    Add files via upload
    Added comments about how I created text file not using write.table.

--- run_analysis.R
+++ run_analysis.R
@@ -66,10 +66,13 @@ run_analysis <- function(){
 # all that remains is the final step to average it over subject and activity
 # which is done in the following line      
       final <- mean_std %>% group_by(subject,activity) %>% summarise_each(funs(mean))
+# I have deviated slighly from the instructions becasue I did not l like
+# the format of text file created by write.table
+# Instead I used the capture.output function to output the print .data.frame to a text file     
 # By  changing the width and line maximum options we can print out the data in 
 # a format that is both tidy and visually easy to look at.
-# note we have limited the signifcant digits to 3 since the accuracy and precision 
-# of cmmercial devices is not even that accurate.  Further digits are noise.
+# note I have limited the signifcant digits to 3 since the accuracy and precision 
+# of commercial devices is not even that accurate.  Further digits are noise.
 # the result appears in file "tidy.txt"
       options(width=3000)
       options(max.print=30000)
